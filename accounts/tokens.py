3ab829fa2035ecf5a2668ffef36a72b6aed7f6d6
hoimonti70 <mrsiyam61@gmail.com>
2024-08-24 22:19:28 +0600

    remove unnecessary codes

--- accounts/tokens.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.tokens import PasswordResetTokenGenerator
-from django.utils.encoding import force_str
-from django.utils.http import int_to_base36
-
-class AccountActivationTokenGenerator(PasswordResetTokenGenerator):
-    def _make_hash_value(self, user, timestamp):
-        return f"{user.pk}{timestamp}{user.is_active}"
-
-account_activation_token = AccountActivationTokenGenerator()
