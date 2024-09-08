4489e8ad7b3f89c48084815600ccc69004931642
hoimonti70 <mrsiyam61@gmail.com>
2024-09-08 13:56:45 +0600

    update user models

--- authentication/models.py
+++ authentication/models.py
@@ -1,8 +1,10 @@
-# models.py
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.core.validators import MinLengthValidator
 
 class CustomUser(AbstractUser):
+    username = models.CharField(max_length=10, unique=True)  # Ensure username is unique
+    password = models.CharField(max_length=128, validators=[MinLengthValidator(8)])  # Corrected password field
     company_name = models.CharField(max_length=30, blank=True, null=True)
     logofname = models.ImageField(upload_to='logos/', blank=True, null=True)
     province_code = models.CharField(max_length=2, blank=True, null=True)
@@ -11,7 +13,7 @@ class CustomUser(AbstractUser):
     phone1 = models.CharField(max_length=15, blank=True, null=True)
     phone2 = models.CharField(max_length=15, blank=True, null=True)
     email = models.EmailField(blank=True, null=True)
-    registration_date = models.DateField(auto_now_add=True)  # Automatically set at user creation
+    registration_date = models.DateField(auto_now_add=True)
     current_activation_date = models.DateField(blank=True, null=True)
     expiry_current_activation = models.DateField(blank=True, null=True)
     subscription_type = models.CharField(max_length=1, choices=[('F', 'Free'), ('P', 'Paid')], default='F')
